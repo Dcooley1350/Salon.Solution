c2e3f7ec7b0432b1851f6f98cdbf05ac1fd3dcdb
Devin Cooley <dcooley1350@gmail.com>
2019-10-18 11:44:55 -0700

    Write Index View for Client and Stylist

--- Salon/Controllers/HomeController.cs
+++ Salon/Controllers/HomeController.cs
@@ -11,6 +11,10 @@ namespace Salon.Controllers
 {
     public class HomeController : Controller
     {
-
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
     }
 }
